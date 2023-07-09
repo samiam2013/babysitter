9623b1d67fc1d28a0f78b8ca2fc12d8cf5ef4279
Sam Myres <samiam2013@gmail.com>
2023-07-09 16:28:42 -0500

    update to simultaneously read stderr

--- babysitter.go
+++ babysitter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -13,10 +14,10 @@ import (
 
 func main() {
 	killOn, command, err := parseArgs()
+	if err == errGaveUsage {
+		return
+	}
 	if err != nil {
-		if err == errGaveUsage {
-			return
-		}
 		log.Fatal(err)
 	}
 
@@ -50,33 +51,62 @@ func babysit(
 		errorC <- err
 		return
 	}
-	if err = cmd.Start(); err != nil {
+	stdErr, err := cmd.StderrPipe()
+	if err != nil {
 		errorC <- err
 		return
 	}
-	go listenAndKill(cmd, killOn, stdOut, outputC, errorC)
+	if err := cmd.Start(); err != nil {
+		errorC <- err
+		return
+	}
+	go listenAndKill(cmd, killOn, stdOut, stdErr, outputC, errorC)
 }
 
 func listenAndKill(
-	cmd *exec.Cmd, killOn []byte, stdOut io.Reader,
+	cmd *exec.Cmd, killOn []byte, stdOut, stdErr io.Reader,
 	outputC chan []byte, errC chan error) {
-	for {
-		r := bufio.NewReader(stdOut)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	stdOutBuf := bufio.NewReader(stdOut)
+	stdErrBuf := bufio.NewReader(stdErr)
+
+	go func() {
+		for {
+			readFrom(ctx, cancel, stdOutBuf, errC, outputC, cmd, killOn)
+		}
+	}()
+	go func() {
+		for {
+			readFrom(ctx, cancel, stdErrBuf, errC, outputC, cmd, killOn)
+		}
+	}()
+
+}
+
+func readFrom(ctx context.Context, cancel context.CancelFunc,
+	r *bufio.Reader, errC chan error, outputC chan []byte,
+	cmd *exec.Cmd, killOn []byte) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
 		line, _, err := r.ReadLine()
 		if err != nil {
 			errC <- err
 			return
 		}
+
 		outputC <- line
-		if !bytes.Contains(bytes.ToLower(line), bytes.ToLower(killOn)) {
-			continue
+
+		if bytes.Contains(bytes.ToLower(line), bytes.ToLower(killOn)) {
+			outputC <- []byte("BabySitter: found string, sending kill signal")
+			if err := cmd.Process.Kill(); err != nil {
+				errC <- err
+			}
+			cancel()
+			close(outputC)
 		}
-		outputC <- []byte("BabySitter: found string, sending kill signal")
-		if err := cmd.Process.Kill(); err != nil {
-			errC <- err
-		}
-		close(outputC)
-		return
 	}
 }
 
@@ -99,6 +129,11 @@ func parseArgs() ([]byte, []string, error) {
 			command = os.Args[i+1:]
 			return []byte(killOn), command, nil
 		}
+	}
+	if len(killOn) == 0 {
+		return nil, nil, fmt.Errorf("no kill_on specified")
+	} else {
+		fmt.Println("kill_on:", killOn)
 	}
 	return nil, nil, fmt.Errorf("no command specified")
 }
--- cmd/halts/main.go
+++ cmd/halts/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -12,6 +13,9 @@ func main() {
 		fmt.Println("insert some api call here")
 		time.Sleep(time.Second * 1)
 		fmt.Println("insert crazy amounts of crunch here")
+		if _, err := fmt.Fprintln(os.Stderr, "error channel output here"); err != nil {
+			fmt.Println("Error writing to stderr")
+		}
 		time.Sleep(time.Second * 1)
 		fmt.Println("Successfully complete stop finished iteration " +
 			"sleeping for 3 seconds")
